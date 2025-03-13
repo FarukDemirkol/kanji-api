75dc2f6053085cc7dbbe640a963b6ffb6ae543d9
Faruk Demirkol <farukdemirkolfd@hotmail.com>
2025-03-14 01:17:24 +0300

    updated

--- server.js
+++ server.js
@@ -8,32 +8,34 @@ app.use(express.json());
 app.use(cors());
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, // Supabase bağlantı URL'in
-  ssl: { rejectUnauthorized: false },
+    connectionString: process.env.DATABASE_URL, // Supabase bağlantı URL'in
+    ssl: { rejectUnauthorized: false },
 });
+
+console.log("Server running on port 8080")
 
 // Kanji listesi çekme
 app.get("/kanji", async (req, res) => {
-  try {
-    const result = await pool.query("SELECT * FROM Kanji");
-    res.json(result.rows);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+    try {
+        const result = await pool.query("SELECT * FROM Kanji");
+        res.json(result.rows);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 });
 
 // Belirli bir kanji çekme
 app.get("/kanji/:id", async (req, res) => {
-  try {
-    const result = await pool.query("SELECT * FROM Kanji WHERE id = $1", [req.params.id]);
-    res.json(result.rows[0]);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+    try {
+        const result = await pool.query("SELECT * FROM Kanji WHERE id = $1", [req.params.id]);
+        res.json(result.rows[0]);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 });
 
 // API'yı dinleme
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
