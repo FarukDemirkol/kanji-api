0a3ee65ad02e6e80f418f6de04ec660351800fba
Faruk Demirkol <farukdemirkolfd@hotmail.com>
2025-03-14 02:37:47 +0300

    route eklendi

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const pool = new Pool({
     ssl: { rejectUnauthorized: false },
 });
 
-console.log("Server running on port 8080")
+console.log("Server running on port 5000")
 
 // Kanji listesi çekme
 app.get("/kanji", async (req, res) => {
@@ -24,6 +24,10 @@ app.get("/kanji", async (req, res) => {
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
+});
+
+app.get("/", (req, res) => {
+    res.send("Kanji API'ya hoş geldiniz!");
 });
 
 // Belirli bir kanji çekme
@@ -37,7 +41,7 @@ app.get("/kanji/:id", async (req, res) => {
 });
 
 // API'yı dinleme
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
