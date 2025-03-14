53ef3a1dac728b095ca3632a7014ffa74ab4854e
Faruk Demirkol <farukdemirkolfd@hotmail.com>
2025-03-14 03:07:53 +0300

    /lessons eklendi

--- server.js
+++ server.js
@@ -37,6 +37,15 @@ app.get("/kanji/:id", async (req, res) => {
     }
 });
 
+app.get("/lessons", async (req, res) => {
+    try {
+        const result = await pool.query("SELECT * FROM Lessons"); // Lessons tablosundaki tüm verileri çek
+        res.json(result.rows);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
 // API'yı dinleme
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
